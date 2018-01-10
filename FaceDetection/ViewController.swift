99f0599d1ee5041ed6285ad600795e25efd5bdae
Ram <p.ramasubramanian@gmail.com>
2018-01-10 23:07:55 +0100

    selected image from photo library

--- FaceDetection/ViewController.swift
+++ FaceDetection/ViewController.swift
@@ -8,11 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var inputField: UITextField!
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        imagePicker.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,27 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func detectFace(_ sender: Any) {
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .savedPhotosAlbum
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let selImage:UIImage = info[UIImagePickerControllerOriginalImage] as? UIImage{
+            imageView.image = selImage
+            imageView.contentMode = .scaleAspectFill
+            
+         let scaledHeight = view.frame.width / selImage.size.width * selImage.size.height
+            imageView.frame = CGRect(x: 10, y: 10, width: selImage.size.width, height: scaledHeight)
+            
+        }
+         dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
 }
 
